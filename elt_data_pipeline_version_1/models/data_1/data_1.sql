8835a7864fef35617f92d5b926210be1da1dfa45
Nitish <64763185+nitishkarekar@users.noreply.github.com>
2025-12-15 09:59:28 +0530

    Update data_1.sql

--- elt_data_pipeline_version_1/models/data_1/data_1.sql
+++ elt_data_pipeline_version_1/models/data_1/data_1.sql
@@ -5,4 +5,4 @@ SELECT
     *,
     -- This adds a timestamp column to every row
     CURRENT_TIMESTAMP as loaded_at_utc
-FROM read_parquet('s3://latest_ev_data.parquet')
+FROM read_parquet('s3://ev-pipeline-fresh-v1/latest_ev_data.parquet')
